122a1438918443149f5e53493cc360d9391064a2
manjushrigokhale <manjushree183@gmail.com>
2021-02-25 17:12:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,5 +4,7 @@ variable "public_key_path" {}
 variable "web_instance_type" {}
 variable "db_instance_type" {}
 variable "aws_profile" {}
-variable "user_data" {}
+data "template_file" "user_data" {
+  template = file("files/user-data.sh")
+}
 
