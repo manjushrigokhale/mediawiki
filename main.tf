b680895ee415cdaaf20b1457ab867a2a73c1bdc9
manjushrigokhale <manjushree183@gmail.com>
2021-02-25 15:58:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "mediawiki" {
 
 # Subnets #
 resource "aws_subnet" "public_1" {
-  vpc_id = "aws_vpc.mediawiki.id"
+  vpc_id = aws_vpc.mediawiki.id
   availability_zone = "us-east-1a"
   cidr_block = "192.168.0.0/24"
   map_public_ip_on_launch = true
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_1" {
 }
 
 resource "aws_subnet" "public_2" {
-  vpc_id = "aws_vpc.mediawiki.id"
+  vpc_id = aws_vpc.mediawiki.id
   availability_zone = "us-east-1b"
   cidr_block = "192.168.1.0/24"
   map_public_ip_on_launch = true
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_2" {
 }
 
 resource "aws_subnet" "private_1" {
-  vpc_id = "aws_vpc.mediawiki.id"
+  vpc_id = aws_vpc.mediawiki.id
   availability_zone = "us-east-1a"
   cidr_block = "192.168.2.0/24"
   
@@ -42,7 +42,7 @@ resource "aws_subnet" "private_1" {
 
 #internet gateway#
 resource "aws_internet_gateway" "media_igw" {
-  vpc_id ="aws_vpc.mediawiki.id"
+  vpc_id = aws_vpc.mediawiki.id
   
   tags = {
       Name = "mediawiki-igw"
@@ -51,7 +51,7 @@ resource "aws_internet_gateway" "media_igw" {
 
 #route tables#
 resource "aws_route_table" "rt_public" {
-  vpc_id = "aws_vpc.mediawiki.id"
+  vpc_id = aws_vpc.mediawiki.id
   route{
       cidr_block = "0.0.0.0/0"
       gateway_id = "aws_internet_gateway.media_igw.id"
