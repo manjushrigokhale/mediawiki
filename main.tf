e6a1bdcba27b460c1e345e2f915959aad177bebf
manjushrigokhale <manjushree183@gmail.com>
2021-02-25 11:05:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -190,7 +190,7 @@ resource "aws_nat_gateway" "nat" {
 
 
 resource "aws_route" "private_route_nat" {
-  route_table_id         = aws_route_table.private_routes_nat.id
+  route_table_id         = aws_route_table.rt_private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
 
